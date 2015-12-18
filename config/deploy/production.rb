25c7cc3ec6478da3abcb6d07a98039f02ddd7283
lelix <smoke.torez@gmail.com>
2015-12-18 12:25:44 +0200

    new-server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 set :stage, :production
-server '52.35.198.134', user: 'centos', roles: %w{web app db}
+#server '52.35.198.134', user: 'centos', roles: %w{web app db}
+server '52.11.161.224', user: 'ubuntu', roles: %w{web app db}
 set :deploy_to, "/var/www/test"
 set :branch, "development"
 set :pty, false
